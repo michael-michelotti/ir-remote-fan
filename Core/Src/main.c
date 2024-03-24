13fd589978cdc5d1e652641a4c7bdc4adbdf3258
Michael Michelotti <50560169+michael-michelotti@users.noreply.github.com>
2024-03-23 22:14:40 -0700

    remove old product images, add new images and gif

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -535,7 +535,7 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_Init(MEMS_INT2_GPIO_Port, &GPIO_InitStruct);
 
   /* EXTI interrupt init*/
-  HAL_NVIC_SetPriority(EXTI9_5_IRQn, 0, 0);
+  HAL_NVIC_SetPriority(EXTI9_5_IRQn, 2, 0);
   HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
 
 /* USER CODE BEGIN MX_GPIO_Init_2 */
